f91660c3872ca8a7bc6d754f717908a77fc07965
hazirahsyafiqah06 <hazirahsyafiqah84@gmail.com>
2026-09-06 13:43:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -464,27 +464,6 @@ function checkBirthdaySurprise() {
 
 function openBirthdaySurpriseOnce(person) {
 
-    const today = new Date();
-
-    const todayKey =
-        today.getFullYear() +
-        "-" +
-        String(today.getMonth() + 1).padStart(2, "0") +
-        "-" +
-        String(today.getDate()).padStart(2, "0");
-
-    const storageKey =
-        "birthdaySurpriseShown_" +
-        person +
-        "_" +
-        todayKey;
-
-    if (sessionStorage.getItem(storageKey)) {
-        return;
-    }
-
-    sessionStorage.setItem(storageKey, "true");
-
     setTimeout(() => {
         openBirthdaySurprise(person);
     }, 800);
