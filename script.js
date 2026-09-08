182bf4f9a40c3babdd3aabac067cc45a89c0e7f8
hazirahsyafiqah06 <hazirahsyafiqah84@gmail.com>
2026-09-08 09:43:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1048,9 +1048,30 @@ function createBirthdayGifts() {
 
 }
 
+
 // ======================================================
 // NOTES
 // ======================================================
+function formatNoteDate(dateString) {
+
+    if (!dateString)
+        return "Date unavailable";
+
+    const date = new Date(dateString);
+
+    return date.toLocaleString(
+        "en-MY",
+        {
+            day: "2-digit",
+            month: "short",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true,
+            timeZone: "Asia/Kuala_Lumpur"
+        }
+    );
+}
 
 async function loadNotes() {
 
