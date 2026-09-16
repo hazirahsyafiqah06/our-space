c66f511ce9fd3fd50fce1bf218a7e148dae038e8
hazirahsyafiqah06 <hazirahsyafiqah84@gmail.com>
2026-09-16 14:28:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2773,17 +2773,20 @@ async function sendSecretMessage() {
     "<strong>Message:</strong><br>" +
     escapeHTML(message)
 );
+    // ======================================================
+// FIREBASE NOTIFICATION
+// ======================================================
+
+await addNotification(
+    receiverId,
+    "💌 New Secret Message",
+    `${USER_NAMES[currentUser.id] || "Your love"} sent you a secret message ❤️`,
+    "secret_message"
+);
 
     await loadSecretMessages();
 
 }
-await sendEmailNotification(
-    "New Secret Message 💌",
-    "💌 New Secret Message",
-    "You have received a new secret message in Our Space.<br><br>" +
-    "<strong>Message:</strong><br>" +
-    escapeHTML(message)
-);
 
 
 // ======================================================
